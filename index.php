6dfaedeb10ac4dfce0d17ab0bd555756d782abf0
JordyMenten <43515087+JordyMenten@users.noreply.github.com>
2019-10-25 19:23:01 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,26 +1,30 @@
 <?php
 echo gethostname();
 echo "<br>";
-\$password = "${password}";
-\$servername = "${mysqlip}";
-\$username = "apache";
-\$dbname = "employees";
+$password = "getenv('Password')";
+$servername = "getenv('Servername')";
+$username = "apache";
+$dbname = "employees";
 
-\$conn = new mysqli(\$servername, \$username, \$password, \$dbname);
-if (\$conn->connect_error) {
-    die("Connection failed: " . \$conn->connect_error);
+$conn = new mysqli($servername, $username, $password, $dbname);
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
 }
 
-\$sql = "SELECT emp_no, first_name, last_name FROM employees WHERE emp_no <= 10050";
-\$result = \$conn->query(\$sql);
+$sql = "SELECT emp_no, first_name, last_name FROM employees WHERE emp_no <= 10050";
+$result = $conn->query($sql);
 
-if (\$result->num_rows > 0) {
-    while(\$row = \$result->fetch_assoc()) {
-        echo "Employee number: " . \$row["emp_no"]. " - Name: " . \$row["first_name"]. " " . \$row["last_name"]. "<br>";
+if ($result->num_rows > 0) {
+    while($row = $result->fetch_assoc()) {
+        echo "Employee number: " . $row["emp_no"]. " - Name: " . $row["first_name"]. " " . $row["last_name"]. "<br>";
     }
 } else {
     echo "0 results";
 }
 
-\$conn->close();
+$conn->close();
 ?>
+
+<html>
+<img src="https://pe-image-bucket.s3.amazonaws.com/test-pattern-152459_960_720.png?fbclid=IwAR1udSaMlCJc5bBu48zESNI_dxc8o3LrARUZMSEx5i9K_uglrNfxLSNWyqE">
+</html>
